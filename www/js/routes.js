5eb6b8c7870954eb039f969ed89b0e5641f0893c
Santosh Khanal <santosh.khanal55@gmail.com>
2016-01-07 15:11:43 +0545

    Ionic Material Theme Added

---
+++ www/js/routes.js
@@ -0,0 +1,55 @@
+angular.module('app.routes', [])
+.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+
+    // Turn off caching for demo simplicity's sake
+    $ionicConfigProvider.views.maxCache(0);
+
+    /*
+    // Turn off back button text
+    $ionicConfigProvider.backButton.previousTitleText(false);
+    */
+
+    $stateProvider.state('app', {
+        url: '/app',
+        abstract: true,
+        templateUrl: 'templates/menu.html',
+        controller: 'AppCtrl'
+    })
+
+    .state('app.login', {
+        url: '/login',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/login.html',
+                controller: 'LoginCtrl'
+            },
+            'fabContent': {
+                template: ''
+            }
+        }
+    })
+
+    .state('app.profile', {
+        url: '/profile',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/profile.html',
+                controller: 'ProfileCtrl'
+            }
+        }
+    })
+
+    .state('app.profilesetting', {
+        url: '/profilesetting',
+        views: {
+            'menuContent': {
+                templateUrl: 'templates/profilesetting.html',
+                controller: 'ProfileSettingCtrl'
+            }
+        }
+    })
+    ;
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/app/login');
+});
